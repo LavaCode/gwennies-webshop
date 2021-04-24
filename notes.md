7f60b2e398dc0ff98e6b5f48f050c207029be36b
LavaCode <kasper.zegel@gmail.com>
2021-04-24 15:10:18 +0200

    added notes and social

---
+++ notes.md
@@ -0,0 +1,12 @@
+## Later priority
+
+1. Multilangual(flag in footer) 
+2. 
+
+
+## Todo's
+
+1. Instagram logo
+    - alignment footer
+    - placement
+2. responsive navbar 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import Tile from '../../components/Tile/Tile';
+import data from '../../content/data.json';
 
 function Home() {
     return (
-        <div>
-            <h1>hello</h1>
+        <div className="home-container">
+            <Tile
+                className="tile-01"
+                title="BRAND"
+                text="Mollit magna fugiat et ex pariatur duis deserunt. Aliquip quis excepteur consectetur labore aute minim do sunt aliquip exercitation. Adipisicing officia pariatur ea ut ut aute eu nostrud laborum adipisicing. Irure aliquip nostrud cupidatat adipisicing consectetur exercitation velit. Anim nulla qui cillum sunt qui sint cillum est labore laboris."
+            />
+            <Tile
+                className="tile-02"
+                // image={brand}
+            />
+            <Tile
+                className="tile-03"
+                // image={story}
+                />
+            <Tile
+                className="tile-04"
+                title="OUR STORY"
+                >
+          <p className="footerText">Mollit magna fugiat et ex pariatur duis deserunt. Aliquip quis excepteur consectetur labore aute minim do sunt aliquip exercitation.</p>
+          <p className="footerText">Yolo diabolo</p>
+          </Tile>
         </div>
     )
 }
